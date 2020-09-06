69cf8ce162c99f0ffa6ab6447d0b4327fe7af92e
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-09-06 19:56:06 +0530

    Initial Commit

---
+++ Farawlah/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Farawlah
+//
+//  Created by Sukumar Anup Sukumaran on 06/09/20.
+//  Copyright © 2020 Tech_Tonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FarawlahTests/FarawlahTests.swift
@@ -0,0 +1,34 @@
+//
+//  FarawlahTests.swift
+//  FarawlahTests
+//
+//  Created by Sukumar Anup Sukumaran on 06/09/20.
+//  Copyright © 2020 Tech_Tonic. All rights reserved.
+//
+
+import XCTest
+@testable import Farawlah
+
+class FarawlahTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
